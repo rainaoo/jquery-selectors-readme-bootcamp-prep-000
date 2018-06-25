a67c1220f016baaf8c5c9446ad9c8e99b8b7c85d
raina <rainaoo2017@gmail.com>
2018-06-25 11:48:06 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,11 @@
 // declare your functions here...
+function paragraphSelector(){
+  document.guerySelectorAll$("p")
+}
+function lastImageSelector(){
+  $('img:last')
+}
+
+function lastImageSelector(){
+  $('img:last')
+}
